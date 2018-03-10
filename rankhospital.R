cfd0ebf5725c3d82ea56d7ceaf61ccfbf63546ab
benirvin <benirvin@comcast.net>
2018-03-09 22:40:55 -0500

    Completed 2nd function rankhospital.R

---
+++ rankhospital.R
@@ -0,0 +1,20 @@
+rankhospital <- function(state, outcome, num = "best") {
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## Return hospital name in that state with the given rank
+    ## 30-day death rate
+    if (!state %in% state.abb)
+        stop("invalid state")
+    if (!outcome %in% c("heart attack", "heart failure", "pneumonia"))
+        stop("invalid outcome")
+    if (outcome=="heart attack") dfcol <-11
+        else if (outcome=="heart failure") dfcol <-17
+            else dfcol <-23
+    olist<-order(outcomes[outcomes$State==state,dfcol],
+                 outcomes[outcomes$State==state,2],na.last=NA)
+    clean_oc <-na.omit(outcomes[outcomes$State==state,dfcol])
+    if (num=="best") num <-1
+        else if(num=="worst") num <- length(clean_oc)
+    outcomes[outcomes$State==state,2][olist[num]]
+
+}
